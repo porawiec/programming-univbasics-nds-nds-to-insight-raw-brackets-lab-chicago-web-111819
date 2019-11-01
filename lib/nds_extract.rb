e341be84390b76fede541392e498ca497c7cdc0f
Paul O <paul.orawiec@gmail.com>
2019-11-01 07:55:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,14 +7,17 @@ def directors_totals(nds)
   pp nds
   #row_index is each hash element in array of directors
   row_index = 0
+  name = []
+  adding_money = 0
+  money = []
   results = {}
   while row_index < nds.length do
     #column_index is each hash element in array of movies
     column_index = 0
-    name = nds[row_index][:name]
+    name.push(nds[row_index][:name])
     grand_total_dir = 0
     while column_index < nds[row_index].length do
-      
+      adding_money =+
     column_index +=1
     end
   
